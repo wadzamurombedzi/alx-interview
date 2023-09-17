5acdf37ca7993fbec326e1d74b0a4ac509639815
wadzamurombedzi <wadzanaimurombedzi0@gmail.com>
2023-09-17 21:54:35 +0000

    Starwars

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,40 +2,40 @@
 const request = require('request');
 
 if (process.argv.length !== 3) {
-	  console.log('Usage: ./0-starwars_characters.js <n>');
-	  process.exit(1);
+  console.log('Usage: ./0-starwars_characters.js <n>');
+  process.exit(1);
 }
 
 const id = process.argv[2];
 if (isNaN(id)) {
-	  console.log('<n> must be an integer');
-	  process.exit(1);
+  console.log('<n> must be an integer');
+  process.exit(1);
 }
 
 function promiseRequest (url) {
-	  return new Promise((resolve, reject) => {
-		      request.get(url, (error, response, body) => {
-			            if (error) {
-					            reject(error);
-					          }
-			            resolve(JSON.parse(body));
-			          });
-		    });
+  return new Promise((resolve, reject) => {
+    request.get(url, (error, response, body) => {
+      if (error) {
+        reject(error);
+      }
+      resolve(JSON.parse(body));
+    });
+  });
 }
 
 async function printChars () {
-	  const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
-	  try {
-		      const response = await promiseRequest(url);
-		      const characters = response.characters;
-		      for (const character of characters) {
-			            const body = await promiseRequest(character);
-			            const name = body.name;
-			            console.log(name);
-			          }
-		    } catch (err) {
-			        console.error(err);
-			      }
+  const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
+  try {
+    const response = await promiseRequest(url);
+    const characters = response.characters;
+    for (const character of characters) {
+      const body = await promiseRequest(character);
+      const name = body.name;
+      console.log(name);
+    }
+  } catch (err) {
+    console.error(err);
+  }
 }
 
 printChars();
